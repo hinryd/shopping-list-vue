c0447eadd18e109bdb3829d48411b1fc7967def1
Henry <lkhh@protonmail.com>
2021-01-02 19:46:43 +0800

    Enable history mode and fix links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/one">About</router-link>
-    <router-link to="/two">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/one">One</router-link> |
+    <router-link to="/two">Two</router-link>
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import ProductListOne from "../components/ProductListOne";
 import ProductListTwo from "../components/ProductListTwo";
@@ -31,7 +31,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  mode: "history",
+  history: createWebHistory(),
   routes,
 });
 
